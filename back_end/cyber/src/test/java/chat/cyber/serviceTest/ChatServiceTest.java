21b62812a7d97a8900657b87204f84607a12ddbc
euler <eullerborgesdamotta155@gmail.com>
2025-09-12 23:29:59 -0300

    fazendos os testes do ChatService

--- back_end/cyber/src/test/java/chat/cyber/serviceTest/ChatServiceTest.java
+++ back_end/cyber/src/test/java/chat/cyber/serviceTest/ChatServiceTest.java
@@ -1,4 +1,44 @@
 package chat.cyber.serviceTest;
 
+import chat.cyber.entity.User;
+import chat.cyber.repository.ChatRepository;
+import chat.cyber.repository.UserRepository;
+import chat.cyber.service.AuthService;
+import chat.cyber.service.ChatService;
+import chat.cyber.service.JwtService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.mockito.Mockito.mock;
+
+@DataJpaTest
+@ActiveProfiles("test")
 public class ChatServiceTest {
+
+    @Autowired
+    private ChatRepository chatRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private ChatService chatService;
+
+    @BeforeEach
+    void setup() {
+        chatService = new ChatService(chatRepository, userRepository);
+    }
+
+    /// createChat
+
+    @Test
+    @DisplayName("Deve retornar erro por não encontrar Emails")
+    void MustReturnErrorForNotFindEmails(){
+
+        User userGet
+
+    }
 }
